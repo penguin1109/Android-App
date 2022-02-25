5454ef9a8237fc81c8c3583d43ff00cf8ec77986
penguin1109 <59923223+penguin1109@users.noreply.github.com>
2022-02-25 14:37:21 +0900

    add_main

---
+++ Hello/app/src/main/java/ixxi/org/MainActivity.java
@@ -0,0 +1,14 @@
+package ixxi.org;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
